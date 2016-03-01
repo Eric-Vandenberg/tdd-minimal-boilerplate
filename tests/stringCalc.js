928839e07c2b1af319479669968a1914b5fd907a
Eric Vandenberg <Eric1827v@gmail.com>
2016-02-29 23:18:58 -0800

    Test for empty string

--- tests/stringCalc.js
+++ tests/stringCalc.js
@@ -6,6 +6,11 @@ suite('test string calculator', function() {
 		this.stringCalc = new StringCalc();
 	});
 
+	test('Empty string or white space only equals string of 0', function() {
+		this.stringCalc.intAdd('');
+		assert.strictEqual(this.stringCalc.defaultProp, 0);
+	});
+
 	test('String of 1 equals string of 1', function() {
 		this.stringCalc.intAdd('1');
 		assert.strictEqual(this.stringCalc.defaultProp, '1');
